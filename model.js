dfa0700b39aab3d026da34166236ebefa63c9fdd
dream-azure <dream_azure@mail.ru>
2012-11-22 23:55:00 +0300

    ...

---
+++ model.js
@@ -0,0 +1,43 @@
+var path = require('path'),
+    misc = require('./misc');
+
+var model = {};
+
+model.serverRoot = path.join(__dirname, 'www');
+
+model.hostPort = 81;
+
+model.hostName = 'localhost:' + model.hostPort;
+
+model.dynamicExt = '.html';
+
+model.indexName = 'index' + model.dynamicExt;
+
+model.isDynamicFile = function(file){
+    return (path.extname(file) === model.dynamicExt);
+};
+
+model.getFsPath = function(rPath){
+    return path.join(model.serverRoot, rPath);
+};
+
+
+model.getFileData = function(file){
+var markdown = require('markdown').markdown;
+        var fs = require("fs");
+        var str = fs.readFileSync(file, "utf8");
+        var a = markdown.toHTML(str);
+
+   
+    return {
+        'content': a,
+        'title': path.basename(file, model.dynamicExt),
+        'host': model.hostName
+    };
+};
+
+model.templateFile = model.getFsPath('/template' + model.dynamicExt);
+
+model.template = misc.getFile(model.templateFile);
+
+module.exports = model;
---
+++ view.js
@@ -0,0 +1,19 @@
+var model = require('./model');
+
+var markdown = require('markdown').markdown;
+
+var view = {};
+
+view.render = function(data) {
+    var template = model.template;
+    Object.keys(data).forEach
+    (
+    function(key)
+        {
+         template = template.replace(new RegExp('%'+key+'%', 'g'), markdown.toHTML(data[key]));
+        }
+    );
+    return template;
+};
+
+module.exports = view;
